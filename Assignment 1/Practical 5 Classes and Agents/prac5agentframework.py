bdcf42e53009aa69cbacbda99987d097ed0b1545
nastazja <nl381@cantab.ac.uk>
2020-10-27 16:19:04 +0000

    Create prac5agentframework.py
    Practical 5 Framework

---
+++ Assignment 1/Practical 5 Classes and Agents/prac5agentframework.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 27 16:05:56 2020
+
+@author: nastazjalaskowski
+"""
+import random
+
+# Set up a Class.
+class Agent:
+    
+# Set up variables x and y for the agent (random numbers).
+    def __init__(self):
+        self.y = random.randint(0,99)
+        self.x = random.randint(0,99)
+        
+# Make a move method within Agent class. Must be in its own place (no indent).           
+    def move(self):
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 100 
+        else:
+            self.y = (self.y - 1) % 100
+
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 100
+        else:
+            self.x = (self.x - 1) % 100
