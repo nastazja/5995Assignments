b70d6a59eacf2ee125d46b724857934010e70ad7
nastazja <nl381@cantab.ac.uk>
2020-11-03 20:34:36 +0000

    Update enhanced_agentframework.py

--- Assignment 1/Final Enhanced Model/enhanced_agentframework.py
+++ Assignment 1/Final Enhanced Model/enhanced_agentframework.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Practical 9 Agent Class Framework, based on GEOG5995 Learning Materials
+Enhanced Agent Class Framework, based on GEOG5995 Learning Materials
 
 @author: nastazjalaskowski
 """
@@ -19,7 +19,7 @@ store: contains the 'food'(data) the agent has eaten.
 neighbourhood: the distance agents need to be within each other to share stored 'food'.
 
 
-The functions define the agent's behaviour. 
+The methods below define the agent's behaviour. 
 
 move: x and y coordinates of the agent change randomly by + or - 1. 
 eat: agents deplete 'food' (data) from the environment and add to their own store at their location.
@@ -95,6 +95,77 @@ class Agent:
                     self.store = avg
                     agent.store = avg
                 #print("sharing " + str(self.distance_between(agent)) + " " + str(avg))
+     
+# The __str__ method allows easy printing of agent locations and stores. 
 
+    def __str__(self):
+        return (str(self.x) + "," + str(self.y) + " " + "stores" + " " + str(self.store))
                 
+    
+
+'''
+The code below contains a Wolves class to define a wolf in the animation. 
+
+y/x: the coordinates of the wolf. 
+store: contains the data the wolf absorbs after eating an agent. 
+wolf_hood: the distance wolves need to be in relation to agents to eat them. 
+
+
+The methods below define the wolf's behaviour. 
+
+move: x and y coordinates of the wolf change randomly by + or - 1. 
+eat_agent: the wolf removes an agent from the environment and adds the agent's store to its own.
+   
+'''
+ 
+
+# Set up the Class for wolves using a constructor method. The y and x coordinates
+# are a random integer between 0 and 300 that change by + or - 1 randomly with each
+# iteration. 
+               
+class Wolves:
+    def __init__(self, wolves, wolf_hood, agents):
+        self.y = (random.randint(0,300))
+        self.x = (random.randint(0,300))
+        self.wolves = wolves
+        self.wolf_hood = wolf_hood
+        self.agents = agents
+        self.store = 0
+    
+    def move(self):
+        if random.random() < 0.5:
+            self.y = (self.y + 1) % 300 # % is the Torus boundary solution. 
+        else: 
+            self.y = (self.y - 1) % 300 
+            
+        if random.random() < 0.5:
+            self.x = (self.x + 1) % 300
+        else:
+            self.x = (self.x - 1) % 300
+            
+# The eat_agent method evaluates whether agents are in the wolf's neighbourhood
+# ie. the 'wolf_hood' and if so, the agent is removed and the wolf absorbs its stores.
+            
+    def eat_agent(self, wolves, agents, wolf_hood):
+     for wolf in self.wolves:
+        for agent in self.agents:
+            wolves_dist = self.distance_between(agent)
+            if wolves_dist <= wolf_hood:
+                self.store += agent.store
+                agents.remove(agent)
+                    
+# Distance between the wolf and agent is measured using Pythagoras theorum. 
+
+    def distance_between(self, agent):
+        return (((self.x - agent.x)**2) + (self.y - agent.y)**2)**0.5 
+    
+# The __str__ method allows easy printing of wolf locations and stores. 
+
+    def __str__(self):
+        return (str(self.x) + "," + str(self.y) + " " + "stores" + " " + str(self.store))
+
+
+'''
+End of Enhanced Agent Class Framework.
+'''
                 
